7b487db8b979075266642bfa1c1f0e3a9d82b887
hafizhsul <hafizhbachtiyar123@gmail.com>
2026-09-02 14:52:08 +0700

    chore(deps): swap Google Fonts for self-hosted Geist variable fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
+import "@fontsource-variable/geist";
+import "@fontsource-variable/geist-mono";
 import "./index.css";
 import { Toaster } from "./components/ui/sonner";
 import Landing from "./pages/Landing";
